f0477d1aea2d28d950a2321f8db459881955265b
Humphrey Codrington <Humphrey.codrington@gmail.com>
2019-11-14 08:27:23 +0000

    pushing rest of second challenege

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+
+  # get    "restaurants/:id/edit",to: "restaurants#edit", as: :edit_restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+booya = Restaurant.create(name: "Booya", address: "London", phone_number: "0987654321", category: "chinese")
+nandos = Restaurant.create(name: "Nandos", address: "Paris", phone_number: "0987654321", category: "italian")
+zuma = Restaurant.create(name: "Zuma", address: "Shanghai", phone_number: "0987654321", category: "french")
+zizi = Restaurant.create(name: "Zizi", address: "Hong Kong", phone_number: "0987654321", category: "japanese")
+ledbury = Restaurant.create(name: "Ledbury", address: "Milan", phone_number: "0987654321", category: "chinese")
+
+good = Review.create(restaurant: booya, content: "it was amazing", rating: 5)
+bad = Review.create(restaurant: nandos, content: "it was shit", rating: 1)
+fun = Review.create(restaurant: zuma, content: "it was fun", rating: 4)
+ok = Review.create(restaurant: zizi, content: "it was ok", rating: 3)
+average = Review.create(restaurant: ledbury, content: "it was average", rating: 3)
